f611307c40d3dc77e17e8b7783c4dbe3bcfb4f83
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-11-16 04:51:41 +0000

    found longest name from an array of intructor objects

---
+++ longest-name.js
@@ -0,0 +1,22 @@
+function instructorWithLongestName(instructors) {
+  let longest = {name: '', course: ''};
+
+  for(let instructor of instructors) {
+    if(instructor.name.length > longest.name.length) {
+      longest = instructor
+    }
+  }
+  return longest;
+}
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
